38f217b285d492c008b10de1c369cd010569e37f
Ingrid Lootsma <ingrid.lootsma@gmail.com>
2017-06-12 15:02:30 +0200

    PHP versies gemaakt

---
+++ contact.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Wartburgia vrijwilligers</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous" />
+
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div class="container-fluid" style="height: 10em;text-align: center;background-image: url('img/wb-vrijwilligers-header.png');background-position: 50% 50%;">
+        </div>
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+            <div class="container">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Vrijwilligers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="functie.html">Functies</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Informatie</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="contact.html">Contact <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Contact</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <p>Neem direct zelf contact op via de gegevens hiernaast, of vul het formulier hieronder in, dan neemt
+                        iemand van het bestuur zo snel mogelijk contact met je op.</p>
+                    <form>
+                        <div class="form-group">
+                            <label for="input-name">Naam</label>
+                            <input type="text" class="form-control" id="input-name" placeholder="Volledige naam">
+                        </div>
+                        <div class="form-group">
+                            <label for="input-email">E-mailadres</label>
+                            <input type="email" class="form-control" id="input-email" aria-describedby="emailHelp" placeholder="email@domein.nl">
+                            <small id="emailHelp" class="form-text text-muted">Je e-mailadres wordt alleen gebruikt om je een bericht terug te kunnen sturen.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="input-phone">Telefoonnummer</label>
+                            <input type="text" class="form-control" id="input-phone" aria-describedby="phoneHelp" placeholder="06-12345678">
+                            <small id="phoneHelp" class-"form-text text-muted">Je telefoonnummer wordt alleen gebruikt om contact met je te kunnen opnemen over je verstuurde bericht.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="input-subject">Onderwerp</label>
+                            <select class="form-control" id="input-subject">
+                                <option>Functie verenigingszaken bestuurslid</option>
+                                <option>Functie 2</option>
+                                <option>Andere vraag</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="input-description">Omschrijving</label>
+                            <textarea class="form-control" id="input-description" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Versturen</button>
+                    </form>
+                </div>
+                <div class="col-4">
+                    <p>
+                        <img src="img/wartlogo.gif" class="image-fluid" />
+                    </p>
+                    <p>
+                        <strong>Contact gegevens</strong><br/>
+                        <a href="mailt:bestuur@wartburgia.nl">bestuur@wartburgia.nl</a><br/>
+                        <a href="tel:06-12345678">06-12345678</a><br/>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+
+        <div class="container-fluid footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <strong>&copy; Wartburgia 2017</strong><br/><br/>
+                        <a href="functie.html">Vacatures</a><br/>
+                        <a href="index.html">Informatie</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery first, then Tether, then Bootstrap JS. -->
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ functie.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Wartburgia vrijwilligers</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous" />
+
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div class="container-fluid" style="height: 10em;text-align: center;background-image: url('img/wb-vrijwilligers-header.png');background-position: 50% 50%;">
+        </div>
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+            <div class="container">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Vrijwilligers</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="functie.html">Functies <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">Informatie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Verenigingszaken bestuurslid</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>
+                        Als bestuurslid Verenigingszaken ben je verantwoordelijk voor alle zaken binnen Wartburgia die niet direct met het voetbal te maken hebben, zoals
+                        de toernooien, sponsoren, klaverjasavonden, pub-quizzes en andere leuke activiteiten.<br/>
+                    </p>
+                    <h2>Verantwoordelijkheden</h2>
+                    <ul>
+                        <li>Toernooien</li>
+                        <li>Extra (niet voetbal) activiteiten</li>
+                        <li>Sponsoring</li>
+                        <li>Merchandise</li>
+                    </ul>
+                    <h2>Tijdsinspanning</h2>
+                    <p>
+                        <strong>3 uur per week</strong><br/>
+                        1 keer in de maand is er een bestuursvergadering waarbij je aanwezig bent. Verder kun je de tijd zelf indelen.
+                    </p>
+                    <h2>Organisatie</h2>
+                    <p>
+                        Je overlegt met de andere bestuursleden en de mensen in de commissies die de toernooien en sponsoring organiseren.
+                    </p>
+                    <h2>Huidige invulling</h2>
+                    <p>
+                        Dit is een nieuwe functie binnen het bestuur waar nog geen invulling aan gegeven is.
+                    </p>
+                    <p>
+                        <a href="contact.html" class="btn btn-primary" role="button">Aanmelden</a>
+                    </p>
+                </div>
+                <div class="col-4">
+                    <p>
+                        <strong>Huidige invulling</strong><br/>
+                        <img src="img/functie-open.png" class="image-fluid" />
+                        <br/><br/>Vrijwilliger gezocht<br/><br/>
+                        <a href="contact.html" class="btn btn-primary" role="button">Aanmelden</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+
+        <div class="container-fluid footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <strong>&copy; Wartburgia 2017</strong><br/><br/>
+                        <a href="functie.html">Vacatures</a><br/>
+                        <a href="index.html">Informatie</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery first, then Tether, then Bootstrap JS. -->
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    </body>
+</html>
